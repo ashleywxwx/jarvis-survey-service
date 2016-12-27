448a531cf70fdcefbd2b1eb33826c390cacbca4a
Andrew Bell <andrew@recursivechaos.com>
2016-12-27 14:32:16 -0600

    Updates integration tests

--- integration-tests/spec/survey-spec.js
+++ integration-tests/spec/survey-spec.js
@@ -1,13 +1,22 @@
 var request = require("request");
 var base_url = "http://survey-service:3000/api/v1/"
 
-describe("when getting service status", function(done) {
-  it("should return status OK", function() {
-    request.get(base_url+ "status", function(error, response, body) {
-      console.log("Response body: " + body);
-      expect(response.statusCode).toBe(666);
-      expect(body).toBe("{'message':'This should be failing'}");
+describe("Survey service", function() {
+  describe("when getting service status", function() {
+
+    it("Should be true", function(done) {
+      console.log("Hello world");
+      expect(true).toBe(false);
       done();
     });
-  })
+
+    it("should return status OK", function(done) {
+      request.get(base_url+ "status", function(error, response, body) {
+        console.log("Response body: " + body);
+        expect(response.statusCode).toBe(666);
+        expect(body).toBe("{'message':'This should be failing'}");
+        done();
+      });
+    });
+  });
 });
