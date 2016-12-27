4d86a82a7ed0e2e8aa54853a69106a828fb3abbe
Andrew Bell <andrew@recursivechaos.com>
2016-12-26 19:20:00 -0600

    Renames, adds methods

---
+++ survey-service/index.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+var bodyParser = require('body-parser');
+
+// App Configuration
+var port = process.env.PORT || 8080;
+var router = express.Router();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Create Models
+var Survey = require('./app/models/survey');
+
+// Connect to db
+mongoose.connect('mongodb://localhost:27017/jarvis-db');
+
+// Middleware - All Requests
+router.use(function(req, res, next) {
+  // Logging \o/
+  console.log('Request received:', req.method, req.originalUrl);
+  next();
+})
+
+// Configure Routes
+router.get('/', function(req, res) {
+  res.json({ message: 'Hello World!'});
+});
+
+router.route('/surveys')
+  // Create an Survey
+  .post(function(req, res) {
+    console.log("Creating new survey: ", req.body);
+    var survey = new Survey();
+    survey.name = req.body.name;
+
+    // save the Survey and check for errors
+    survey.save(function(err)  {
+      if (err)
+        res.send(err);
+
+      res.json(survey);
+    });
+  })
+  // Get all surveys
+  .get(function(req, res) {
+    console.log("Getting all surveys.");
+    Survey.find(function(err, surveys) {
+      if (err)
+        res.send(err);
+
+      res.json(surveys);
+    })
+  });
+
+router.route('/surveys/:id')
+  // Get the survey with that ID (GET /api/v1/surveys/:id)
+  .get(function(req, res) {
+    Survey.findById(req.params.id, function(err, survey) {
+      if (err)
+        res.send(err);
+
+      res.json(survey)
+    })
+  })
+
+  // Update Survey with that ID
+  .patch(function(req, res) {
+
+    // Use our survey model to find
+    Survey.findById(req.params.id, function(err, survey) {
+      if (err)
+        res.send(err);
+
+      // Update survey
+      survey.name = req.body.name;
+
+      // save the survey
+      survey.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json(survey);
+      })
+
+    })
+  })
+  // Delete survey
+  .delete(function (req, res) {
+    Survey.remove({
+      _id: req.params.id
+    }, function(err, survey) {
+      if (err)
+        res.send(err);
+
+        res.json({ message: req.params.id + " deleted."})
+    })
+  })
+
+// Register Routes
+app.use('/api/v1', router);
+
+// Start server
+app.listen(port);
+console.log('Server started on port ' + port);
